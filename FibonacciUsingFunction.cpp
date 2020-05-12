316beb4b5150c6a7d3cd7c5307af5838cc431a7c
Garima Gautam <53808998+gari1311@users.noreply.github.com>
2020-05-13 00:11:02 +0530

    Create FibonacciUsingFunction.cpp

---
+++ FibonacciUsingFunction.cpp
@@ -0,0 +1,60 @@
+/*
+Given a number N, figure out if it is a member of fibonacci series or not. Return true if the number is member of fibonacci series else false.
+Fibonacci Series is defined by the recurrence
+    F(n) = F(n-1) + F(n-2)
+where F(0) = 0 and F(1) = 1
+
+
+Input Format :
+Integer N
+Output Format :
+true or false
+Constraints :
+0 <= n <= 10^4
+Sample Input 1 :
+5
+Sample Output 1 :
+true
+Sample Input 2 :
+14
+Sample Output 2 :
+false    
+*/
+
+#include<iostream>
+using namespace std;
+#include "Solution.h"
+
+int main(){
+
+  int n; 
+  cin >> n ;
+  if(checkMember(n)){
+    cout << "true" << endl;
+  }else{
+    cout << "false" << endl;
+  }
+
+}
+bool checkMember(int n){
+
+  /* Don't write main().
+   * Don't read input, it is passed as function argument.
+   * Return output and don't print it.
+   * Taking input and printing output is handled automatically.
+  */
+    
+     int x1=1, x2=1, x3;
+    
+    for(int i=1; i<=n; i++){
+        x1=x2;
+        x2=x3;
+        x3=x1+x2;
+        
+        if(n==x3){
+        return true;
+    }
+    }
+    
+    return false;
+}
